8c8af7108c16db5a16c5a80920c81b30d5a3a825
D.Biles <darron.biles@gmail.com>
2018-10-02 12:15:42 -0400

    Complete. MVP finished

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,53 +1,69 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAxXryRfhLycQrmuVRzJKsxvkWTYhQDGTo",
-    authDomain: "trainscheduler-31b14.firebaseapp.com",
-    databaseURL: "https://trainscheduler-31b14.firebaseio.com",
-    projectId: "trainscheduler-31b14",
-    storageBucket: "trainscheduler-31b14.appspot.com",
-    messagingSenderId: "390799876929"
-  };
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAxXryRfhLycQrmuVRzJKsxvkWTYhQDGTo",
+  authDomain: "trainscheduler-31b14.firebaseapp.com",
+  databaseURL: "https://trainscheduler-31b14.firebaseio.com",
+  projectId: "trainscheduler-31b14",
+  storageBucket: "trainscheduler-31b14.appspot.com",
+  messagingSenderId: "390799876929"
+};
 
-  firebase.initializeApp(config);
-  const db = firebase.database();
+firebase.initializeApp(config);
+const db = firebase.database();
 
-  $('#submit-train').click(event => {
-    event.preventDefault();
+let trainName = $("#train-name");
+let destination = $("#destination");
+let firstTrainTime = $("#first-train-time");
+let frequency = $("#frequency");
+let nextArrival;
 
-    let trainName = $('#train-name')
-    let destination = $('#destination')
-    let firstTrainTime = $('#first-train-time')
-    let frequency = $('#frequency')
+$("#submit-train").click(event => {
+  event.preventDefault();
 
-    // Add form inputs to database as a list
-    db.ref().push({
-      trainName: trainName.val().trim(),
-      destination: destination.val().trim(),
-      firstTrainTime: firstTrainTime.val().trim(),
-      frequency: frequency.val().trim(),
-      nextArrival: 1, 
-      minsAway: 1,
-    })
+  // Add form inputs to database as a list
+  db.ref().push({
+    trainName: trainName.val().trim(),
+    destination: destination.val().trim(),
+    firstTrainTime: firstTrainTime.val().trim(),
+    frequency: frequency.val().trim(),
+  });
 
-    // Empty values on the form
-    trainName.val('');
-    destination.val('');
-    firstTrainTime.val('');
-    frequency.val('');
-  })
+  // Empty values on the form
+  trainName.val("");
+  destination.val("");
+  firstTrainTime.val("");
+  frequency.val("");
+});
 
-  // Listener for DB
-  // TODO: Calculate the next arrival
-  // TODO: Calcaulate minutes away
-  db.ref().on('child_added', function(snapshot){
-    console.log(snapshot.val())
-    $('.table-body').append(
-      `<tr>
+// Listener for DB
+// TODO: Calcaulate minutes away
+db.ref().on("child_added", function(snapshot) {
+
+  nextArrival = nextTime(snapshot.val().firstTrainTime, snapshot.val().frequency);
+
+  let minsAway = nextArrival.fromNow(true)
+  $(".table-body").append(
+    `<tr>
         <td> ${snapshot.val().trainName}
         <td> ${snapshot.val().destination}
         <td> ${snapshot.val().frequency} min
-        <td> ${snapshot.val().nextArrival} 
-        <td> ${snapshot.val().minsAway} 
+        <td> ${nextArrival.format('h:mm A')}
+        <td> ${minsAway}
       `
-    )
-  })
+  );
+});
+
+// Find the next arrival time
+function nextTime(firstTrainTime, frequency) {
+  nextArrival = moment(firstTrainTime, "HH:mm");
+  console.log(nextArrival);
+  if (moment().isBefore(nextArrival)) {
+    console.log(`the start time is later than the current time of ${moment()}`);
+    return nextArrival;
+  } else {
+    while (moment().isAfter(nextArrival)) {
+      nextArrival = nextArrival.add(frequency, "m");
+    }
+  }
+  return nextArrival;
+}
